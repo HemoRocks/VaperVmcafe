f2dd19687c5add6112739898c3b2017f6d444337
HemoRocks <125079637+HemoRocks@users.noreply.github.com>
2024-02-09 11:50:36 +0400

    first

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,106 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { useState } from 'react';
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [success, setSuccess]=useState(false);
+  const [invalid, setinvalid] = useState(false);
+  const [textP,settextP] = useState("");
+  const validateBasicEmail = (email) => {
+    return email.includes('@') && email.includes('.');
+  };
+  const validatePhoneNumber = (input_str) => {
+    const re = /^\d{10}$/;
+    return re.test(input_str);
+  }
+
+  const handleSubmit = async () => {
+    if (validateBasicEmail(email) && validatePhoneNumber(phone) ) {
+      const myObject = {"Email":email, "Phone":phone};
+      const jsonString = JSON.stringify(myObject);
+      console.log(jsonString);
+      const response = await fetch('http://localhost:3500/Users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        
+        body: [jsonString],
+      });
+
+      if (response.ok) {
+        // Handle success (e.g., show a confirmation message)
+        console.log('Data saved successfully!');
+        setSuccess(true);
+        //Success();
+      } else {
+        // Handle error
+        console.error('Error saving data.');
+      }
+    } else{
+        setinvalid(true);
+        if(invalid == true){
+          settextP("Either the email or the phone number is invalid")
+        }else{settextP("")}
+    }
+    
+  };
+
+  if (success == false){
+    return (
+      <main className={styles.main}>
+        <p className={styles.title}>Ibrahim's Coffee House</p>
+        <div className={styles.textInputdiv}>
+          <p className={styles.textInputlabel}>Email</p>
+          <input
+            type="email"
+            value={email}
+            placeholder="example@example.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.textInput}
+            autoComplete="on"            
+          />
+          <p className={styles.textInputlabel}>Phone Number</p>
+          <input
+            type="tel"
+            placeholder="0550000000"
+            value={phone}
+            required
+            pattern="[0-9]{10}"
+            onChange={(e) => setPhone(e.target.value)}
+            className={styles.textInput}
+          />
+          <p className={styles.invalid}>{textP}</p>
+          <button onClick={handleSubmit} className={styles.button}>Submit</button>
+        </div>
+      </main>
+    );
+  }else{
+    return (
+      <main className={styles.main2}>
+        <p className={styles.title}>Ibrahim's Coffee House</p>
+        {/* <p className={styles.succeed}>Success</p> */}
+        <div className={styles.imagesgroup}>
+          <div className={styles.images}>
+            <Image src={require('./beans.png')} 
+              className={styles.beans}
               priority
             />
-          </a>
+            <p className={styles.coffee_text}>Fresh roasted coffee beans are the ultimate choice for coffee lovers who want to enjoy the full flavor and aroma of their favorite brew. Roasting coffee beans transforms the green, raw beans into the brown, fragrant ones that we know and love. Roasting also develops the complex chemical compounds that give coffee its distinctive taste and health benefits. Different roasting levels, from light to dark, affect the color, body, acidity, and sweetness of the coffee. Fresh roasted coffee beans should be consumed within a few weeks of roasting, as they lose their freshness and flavor over time. </p>
+          </div>
+          <div className={styles.images}>
+            <p className={styles.coffee_text}> Coffee can be brewed in different ways, such as drip, pour-over, French press, or espresso. Depending on the brewing method, the caffeine content, taste, and aroma of the coffee can vary. A cup of coffee can also be customized with various additions, such as milk, cream, sugar, honey, spices, or syrups.</p>
+            <Image src={require('./cup.png')} 
+              className={styles.cup}
+              priority
+            />
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
